3e06d7d8d0312255a636274a8454397b45e36f3f
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-11-16 23:57:34 +0530

    button commit 1

--- app/src/main/java/com/example/logsi/MainActivity.java
+++ app/src/main/java/com/example/logsi/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.logsi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText user = findViewById(R.id.editTextTextEmailAddress);
+        EditText password = findViewById(R.id.editTextTextPassword);
+
+
     }
 }
